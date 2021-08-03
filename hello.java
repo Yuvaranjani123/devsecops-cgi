8b0d24cb026d284cad69ebfcd1dfd83e7f5a7f63
Yuvaranjani123 <47520340+Yuvaranjani123@users.noreply.github.com>
2021-08-03 11:32:24 +0530

    run both conf and java flow

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
    public static void main(String args[])
    {
-	System.out.println("Hello World ! Start conf file");
+	System.out.println("Hello World ! Start conf and java workflow file");
    }	 
 }
